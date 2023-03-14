b496b63dee02a9245327a9d9f88efe9e469b40e0
alpetrushkin <alpetrushkin1984@mail.ru>
2023-03-14 21:23:07 +0300

    css-modules

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 export const Profile = () => {
    return (
-      <main className="content">
+      <main className={s.content}>
          <img
             src="https://sun9-30.userapi.com/impf/c858120/v858120218/1bb4b6/5wfzdFcNKM4.jpg?size=0x0&quality=90&proxy=1&sign=e3d64951cee3a21f5b33303d7a390b5b&c_uniq_tag=G3n5h_pZ-UyW6rbD90h9JpEh8J3mFGNMtJLC7gEhQXc&type=video_thumb"
-            alt="images"/>
+            alt="images"
+         />
          <div>ava + description</div>
          <div>My posts
             <div>New post</div>
-            <div>Post 1</div>
-            <div>Post 2</div>
+            <div>
+               <div className={s.item}>Post 1</div>
+               <div className={s.item}>Post 2</div>
+            </div>
          </div>
       </main>
    );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 export const Header = () => {
    return (
-      <header className="header">
+      <header className={s.header}>
          <img src="https://comparer-tout.org/wp-content/uploads/2022/03/images-1024x979.png" alt="logo"/>
       </header>
    );

e304a051f0d1e52d657f267402ac32787f118cad
alpetrushkin <alpetrushkin1984@mail.ru>
2023-03-14 20:05:36 +0300

    Разбивка на компоненты

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+   return (
+      <header className="header">
+         <img src="https://comparer-tout.org/wp-content/uploads/2022/03/images-1024x979.png" alt="logo"/>
+      </header>
+   );
+};

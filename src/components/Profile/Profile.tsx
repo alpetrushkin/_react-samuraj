783b0818a9113fb62247542148ed9726a2290516
alpetrushkin <alpetrushkin1984@mail.ru>
2023-03-15 20:18:04 +0300

    18(страница Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,8 +4,8 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
    return (
-      <main className={s.content}>
-         <img
+      <main>
+         <img className={s.profileImg}
             src="https://sun9-30.userapi.com/impf/c858120/v858120218/1bb4b6/5wfzdFcNKM4.jpg?size=0x0&quality=90&proxy=1&sign=e3d64951cee3a21f5b33303d7a390b5b&c_uniq_tag=G3n5h_pZ-UyW6rbD90h9JpEh8J3mFGNMtJLC7gEhQXc&type=video_thumb"
             alt="images"
          />

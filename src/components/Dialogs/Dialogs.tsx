0bd3b6b1fc32887db84f8b960a8d8b977d06d05c
alpetrushkin <alpetrushkin1984@mail.ru>
2023-03-16 18:51:00 +0300

    21(верстка сраницы Dialogs)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
    return (
-      <div>
-         MESSAGES
+      <div className={s.dialogs}>
+         <div className={s.dialog}>
+            <div className={s.item}>Andrew</div>
+            <div className={s.item}>Dmitry</div>
+            <div className={`${s.item} ${s.active}`}>Sasha</div>
+            <div className={s.item}>Sveta</div>
+            <div className={s.item}>Valera</div>
+            <div className={s.item}>Viktor</div>
+         </div>
+         <div className={s.message}>
+            <div className={s.messageItem}>
+               I am a normal popover and
+            </div>
+            <div className={s.messageItem}>
+               Can have text and everything
+            </div>
+            <div className={s.messageItem}>
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </div>
+         </div>
       </div>
    );
 };

d6406c945e05f2a185be46cb63182d547b9b1970
alpetrushkin <alpetrushkin1984@mail.ru>
2023-03-16 20:12:29 +0300

    22(навигация NavLink и класс active)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
    return (
       <div className={s.dialogs}>
          <div className={s.dialog}>
-            <div className={s.item}>Andrew</div>
-            <div className={s.item}>Dmitry</div>
-            <div className={`${s.item} ${s.active}`}>Sasha</div>
-            <div className={s.item}>Sveta</div>
-            <div className={s.item}>Valera</div>
-            <div className={s.item}>Viktor</div>
+            <div className={s.item}>
+               <NavLink to="/dialogs/1" className={({isActive}) => (isActive ? s.active : '')}>Andrew</NavLink>
+            </div>
+            <div className={s.item}>
+               <NavLink to="/dialogs/2" className={({isActive}) => (isActive ? s.active : '')}>Dmitry</NavLink>
+            </div>
+            <div className={s.item}>
+               <NavLink to="/dialogs/3" className={({isActive}) => (isActive ? s.active : '')}>Sasha</NavLink>
+            </div>
+            <div className={s.item}>
+               <NavLink to="/dialogs/4" className={({isActive}) => (isActive ? s.active : '')}>Sveta</NavLink>
+            </div>
+            <div className={s.item}>
+               <NavLink to="/dialogs/5" className={({isActive}) => (isActive ? s.active : '')}>Valera</NavLink>
+            </div>
+            <div className={s.item}>
+               <NavLink to="/dialogs/6" className={({isActive}) => (isActive ? s.active : '')}>Viktor</NavLink>
+            </div>
          </div>
          <div className={s.message}>
             <div className={s.messageItem}>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,21 +5,21 @@ import {NavLink} from "react-router-dom";
 export const Navbar = () => {
    return (
       <nav className={s.nav}>
-            <div>
-               <NavLink to="/profile" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Profile</NavLink>
-            </div>
-            <div>
-               <NavLink to="/dialogs" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Messages</NavLink>
-            </div>
-            <div>
-               <NavLink to="/news" className={({isActive}) => (isActive ? s.activeLink : s.item)}>News</NavLink>
-            </div>
-            <div>
-               <NavLink to="/music" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Music</NavLink>
-            </div>
-            <div>
-               <NavLink to="/settings" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Settings</NavLink>
-            </div>
+         <div>
+            <NavLink to="/profile" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Profile</NavLink>
+         </div>
+         <div>
+            <NavLink to="/dialogs" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Messages</NavLink>
+         </div>
+         <div>
+            <NavLink to="/news" className={({isActive}) => (isActive ? s.activeLink : s.item)}>News</NavLink>
+         </div>
+         <div>
+            <NavLink to="/music" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Music</NavLink>
+         </div>
+         <div>
+            <NavLink to="/settings" className={({isActive}) => (isActive ? s.activeLink : s.item)}>Settings</NavLink>
+         </div>
       </nav>
    );
 };
